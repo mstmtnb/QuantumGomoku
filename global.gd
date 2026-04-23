b07f794044d854b237cc2c9e9b1063beb99140ec
Tanabe <tanabe@masatomi.co.jp>
2026-04-23 16:16:15 +0900

    4_23_1

--- global.gd
+++ global.gd
@@ -2,3 +2,11 @@ extends Node
 
 #ターンを管理,黒=0,白=1
 var turn = 0
+
+#所持ポイントを管理
+var blackPoint = 70
+var whitePoint = 70
+
+#使用ポイントを管理
+var blackUsePoint = 70
+var whiteUsePoint = 70
--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+signal test
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,9 +10,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	$Panel/Panel/Label3.text = str(Global.blackPoint)
 	pass
 	
 func startGame():
 	print("test")
 	Global.turn = 0
+	Global.blackPoint = 70
+	Global.whitePoint = 70
+	
+	
+	pass
+
+func updateBoard():
 	pass
